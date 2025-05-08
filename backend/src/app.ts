aa5565ff783fa1d6e23d22d1ce2cc1cbf13d848b
osten-antonio <ostenantonio1@gmail.com>
2025-05-09 00:16:52 +0700

    itenary interface

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,37 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+interface ItineraryDay {
+    location?: string[];
+    priceRange?: string[];
+    priceLevel?: number[];
+    type?: string[];
+    isGoodForChildren?: boolean[];
+    isGoodForGroups?: boolean[];
+    servesBeer?: boolean[];
+    servesBreakfast?: boolean[];
+    servesBrunch?: boolean[];
+    servesCocktail?: boolean[];
+    servesCoffee?: boolean[];
+    servesDessert?: boolean[];
+    servesDinner?: boolean[];
+    servesLunch?: boolean[];
+    servesVegetarianFood?: boolean[];
+    servesWine?: boolean[];
+    hasLiveMusic?: boolean[];
+    allowsDogs?: boolean[];
+    accessibilityOptions?: string[];
+    miscellaneous?: {
+      time: string;
+      activity: string;
+    }[];
+  }
+  
+interface Itinerary {
+    [day: string]: ItineraryDay;
+}
+
+
 const gemini = new GoogleGenAI({vertexai:false, apiKey: process.env.GEMINI_API_KEY });
 
 
